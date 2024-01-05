379abbb79468c8824820e907d06a917babdefaf7
Suraj Kumar <surajsinhaking77@gmail.com>
2024-01-06 04:11:22 +0530

    done

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,55 +1,55 @@
-import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
-import CredentialsProvider  from 'next-auth/providers/credentials'
+// import NextAuth from "next-auth";
+// import GithubProvider from "next-auth/providers/github";
+// import CredentialsProvider  from 'next-auth/providers/credentials'
 
 
-const authOptions = {
-    providers: [
-      CredentialsProvider({
-        name: "Credentials",
-        credentials :{
-          email: { label: "email", type: "email", placeholder: "name123@gmail.com" },
-          password: { label: "Password", type: "password" }
-        },
-        async authorize(credentials, req) {
+// const authOptions = {
+//     providers: [
+//       CredentialsProvider({
+//         name: "Credentials",
+//         credentials :{
+//           email: { label: "email", type: "email", placeholder: "name123@gmail.com" },
+//           password: { label: "Password", type: "password" }
+//         },
+//         async authorize(credentials, req) {
           
-          console.log(req.body);
-          const user = {
-            id : 1,
-            email: req.body.email,
-          }
+//           console.log(req.body);
+//           const user = {
+//             id : 1,
+//             email: req.body.email,
+//           }
     
-          if (user) {
-            // Any object returned will be saved in `user` property of the JWT
-            return user
-          } else {
-            // If you return null then an error will be displayed advising the user to check their details.
-            return null
-            // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
-          }
-        }
+//           if (user) {
+//             // Any object returned will be saved in `user` property of the JWT
+//             return user
+//           } else {
+//             // If you return null then an error will be displayed advising the user to check their details.
+//             return null
+//             // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+//           }
+//         }
 
-      }),
-      GithubProvider({
-        clientId: process.env.GITHUB_CLIENT_ID,
-        clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      }),
-    ],
-    callbacks: {
-      async jwt(token, user) {
-        if (user) {
-          token.id = user.id;
-          token.email = user.email;
-        }
-        return token;
-      },
-    },
-};
+//       }),
+//       GithubProvider({
+//         clientId: process.env.GITHUB_CLIENT_ID,
+//         clientSecret: process.env.GITHUB_CLIENT_SECRET,
+//       }),
+//     ],
+//     callbacks: {
+//       async jwt(token, user) {
+//         if (user) {
+//           token.id = user.id;
+//           token.email = user.email;
+//         }
+//         return token;
+//       },
+//     },
+// };
 
-export default NextAuth(authOptions);
+// export default NextAuth(authOptions);
 
 
-// GithubProvider({
-//   clientId: "554ef0348ffe2f772683",
-//   clientSecret: "5c2b724c4d4d9773a277967e3eb05a66089aeb68",
-// }),
+// // GithubProvider({
+// //   clientId: "554ef0348ffe2f772683",
+// //   clientSecret: "5c2b724c4d4d9773a277967e3eb05a66089aeb68",
+// // }),
--- pages/dashboard/home/index.jsx
+++ pages/dashboard/home/index.jsx
@@ -1,8 +1,8 @@
-import { useSession} from "next-auth/react"
-import { useRouter } from "next/router";
+// import { useSession} from "next-auth/react"
+// import { useRouter } from "next/router";
 export default function InternalHome(){
-    const {data:session} = useSession();
-    const router = useRouter();
+    // const {data:session} = useSession();
+    // const router = useRouter();
 
     // if(!session){
     //     // router.replace()
