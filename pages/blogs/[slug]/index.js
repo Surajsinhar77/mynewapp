628dea3654d640f5fd826c608e562f49774c9fc0
Suraj Kumar <surajsinhaking77@gmail.com>
2024-01-06 16:09:51 +0530

    done

--- pages/blogs/[slug]/index.js
+++ pages/blogs/[slug]/index.js
@@ -7,7 +7,7 @@ import Image from "next/image";
 import Blogs from "../../components/Blogs";
 import blogData  from "../blogData.json";
 
-export default function Page(){
+export default function Page({data, index}){
     
     const params  = useParams();
     const BlogsContent = blogData;
