ef5ca63b609efc05b9bc8bd92baf1873a9483426
Suraj Kumar <surajsinhaking77@gmail.com>
2024-01-06 14:54:48 +0530

    done

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -43,7 +43,7 @@ export default function Page() {
                         <div className="mainImage grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 m-auto">
                             {
                                 ProjectList.map((project,index)=>{
-                                        return <ProjectCard key={index} project={project} index={index}/>
+                                        return <ProjectCard key={index} projectIndex={index}/>
                                     }
                                 )
                             }
