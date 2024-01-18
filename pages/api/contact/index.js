298aef64de37b9baa0f8a425e2be9a876080c7f0
suraj kumar <surajsinhakingr77>
2024-01-18 14:39:01 +0530

    done

--- pages/api/contact/index.js
+++ pages/api/contact/index.js
@@ -1,3 +1,75 @@
-export default function handler(req, res) {
-    res.status(200).json({ name: 'John Doe' })
+const nodemailer = require('nodemailer')
+
+//Email data from Env file
+const myEmail = process.env.myEmail;
+const myEmailPass = process.env.myEmailPass;
+
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 5000,
+    secure: false,
+    auth: {
+        user: myEmail,
+        pass: myEmailPass,
+    },
+});
+
+export default async function handler(req, res) {
+    const { name, email, subject, message } = req.body.userFormData;
+    try {
+        const mailOptions = {
+            from: email, // sender address
+            to: myEmail, // receiver email
+            subject: `${subject}`, // Subject line
+            text: `Hi there, you were emailed from ${name} <br/> ${message}`,
+            html: `
+                <!DOCTYPE html>
+                <html>
+                <head>
+                <meta charset="utf-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1">
+                <title>${subject}</title>
+                </head>
+                <body>
+                <div style="font-family: Arial, sans-serif; font-size: 16px; color: #333; max-width: 600px; margin: 0 auto;">
+                    <header style="background-color: #f5f5f5; padding: 20px;">
+                    <img src="https://your-logo-url" alt="Your Logo" style="width: 150px;">
+                    </header>
+                
+                    <h3 style="font-size: 24px; margin: 20px 0;">Hello ${name},</h3>
+                
+                    <p style="margin: 20px 0;">
+                        ${message}
+                    </p>
+                
+                    <a href="https://your-link" style="background-color: #007bff; color: #fff; padding: 10px 20px; text-decoration: none; border-radius: 5px; margin: 0 auto;">Click Here</a>
+                
+                    <footer style="background-color: #f5f5f5; padding: 20px; text-align: center;">
+                    <p>© 2023 Afflite MyAmazingStore</p>
+                    </footer>
+                </div>
+                </body>
+                </html>
+            `,
+        }
+
+        const info = await transporter.sendMail(mailOptions)
+        if (info) {
+            console.log("done sending mail");
+            const thank = await transporter.sendMail(
+                {
+                    from: myEmail, // sender address
+                    to: email, // receiver email
+                    subject: "Thank You", // Subject line
+                    text: `Hi there, you were emailed from ${myEmail} `,
+                    html: `<h1> Thank yor From Subscribe ${email} </h1>`,
+                }
+            )
+        }
+        return res.status(200).json({ message: " Mail sucessfull sent" })
+    } catch (err) {
+        console.log(err);
+        return res.status(404).json({ message: err.message });
+    }
 }
--- pages/contact/index.jsx
+++ pages/contact/index.jsx
@@ -13,58 +13,88 @@ export default function Contact() {
         })
     }
 
-    function handelFormSubmit(e) {
+    async function handelFormSubmit(e) {
         e.preventDefault();
         console.log("Does we came here to something");
-        api.post('/user/contactus', { userFormData })
-            .then((response) => {
-                console.log(response);
-                alert(response.data.message);
-            }).catch((err) => {
-                console.log(err);
-            })
-    }
-    return (
-        <>
-            <div className="contactContainer w-9/12 m-auto">
-                <Navbar />
-                <div className="subContainer w-9/12 m-auto max-sm:w-full">
-                    <div className="contactHeader">
-                        <h2 className="text-4xl">Contact Me</h2>
-                        <p className="text-lg mt-4">Thank you for visiting my portfolio, I&rsquo;m excited to connect with you Whether you have a question, a potential project, or just want to say hello, feel free to reach out using the contact form below or through the provided contact details </p>
-                    </div>
-                    <div className="formContainer mt-4 text-gray-500">
-                        <form action="">
-                            <div className="col flex justify-between mt-8 max-lg:flex-col">
-                                <div className="row1 flex flex-col w-full mr-2">
-                                    <label htmlFor="">NAME <span className="text-red-500 ">*</span> </label>
-                                    <input className="focus:outline-none focus:outline-slate-500 focus:border-none rounded-md h-10 border pl-2" type="text" />
-                                </div>
-                                <div className="row1 flex flex-col w-full ml-2  max-lg:ml-0">
-                                    <label htmlFor="">EMAIL <span className="text-red-500 mb-2">*</span></label>
-                                    <input className="focus:outline-none focus:outline-slate-500 focus:border-none rounded-md h-10 border pl-2" type="email" />
-                                </div>
-                            </div>
-                            <div className="col flex w-full mt-8">
-                                <div className="row1 flex flex-col w-full">
-                                    <label htmlFor="">SUBJECT <span className="text-red-500 mb-2">*</span> </label>
-                                    <input className="focus:outline-none focus:outline-slate-500 focus:border-none rounded-md h-10 border pl-2" type="text" />
-                                </div>
-                            </div>
-                            <div className="col flex w-full mt-8">
-                                <div className="row1 flex flex-col w-full h-48">
-                                    <label htmlFor="">Message <span className="text-red-500 mb-2">*</span> </label>
-                                    <textarea className="focus:outline-none focus:outline-slate-500 focus:border-none rounded-md border pl-2" rows='100%'></textarea>
-                                </div>
-                            </div>
-                            <div className="col flex w-full mt-8">
-                                <button className="px-5 py-2 rounded-3xl text-white bg-blue-500 hover:bg-blue-300 hover:text-gray-600">Submit</button>
-                            </div>
-                        </form>
-                    </div>
+        try {
+            // Example of a POST request with JSON data
+            const response = await fetch('http://localhost:300/api/contact', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(userFormData),
+            });
+
+            const result = await response.json();
+            console.log(result);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+return (
+    <>
+        <div className="contactContainer w-9/12 m-auto">
+            <Navbar />
+            <div className="subContainer w-9/12 m-auto max-sm:w-full">
+                <div className="contactHeader">
+                    <h2 className="text-4xl">Contact Me</h2>
+                    <p className="text-lg mt-4">Thank you for visiting my portfolio, I&rsquo;m excited to connect with you Whether you have a question, a potential project, or just want to say hello, feel free to reach out using the contact form below or through the provided contact details </p>
                 </div>
-                <Footer />
+                <div className="formContainer mt-4 text-gray-500">
+                    <form action="">
+                        <div className="col flex justify-between mt-8 max-lg:flex-col">
+                            <div className="row1 flex flex-col w-full mr-2">
+                                <label htmlFor="">NAME <span className="text-red-500 ">*</span> </label>
+                                <input 
+                                    className="focus:outline-none focus:outline-slate-500 focus:border-none rounded-md h-10 border pl-2" 
+                                    type="text" 
+                                    name="name"
+                                    onChange={handelFormData}
+                                />
+                            </div>
+                            <div className="row1 flex flex-col w-full ml-2  max-lg:ml-0">
+                                <label htmlFor="">EMAIL <span className="text-red-500 mb-2">*</span></label>
+                                <input 
+                                    className="focus:outline-none focus:outline-slate-500 focus:border-none rounded-md h-10 border pl-2" 
+                                    type="email"
+                                    name="email"
+                                    onChange={handelFormData}
+                                />
+                            </div>
+                        </div>
+                        <div className="col flex w-full mt-8">
+                            <div className="row1 flex flex-col w-full">
+                                <label htmlFor="">SUBJECT <span className="text-red-500 mb-2">*</span> </label>
+                                <input 
+                                    className="focus:outline-none focus:outline-slate-500 focus:border-none rounded-md h-10 border pl-2" 
+                                    type="text"
+                                    name="subject"
+                                    onChange={handelFormData}
+                                />
+                            </div>
+                        </div>
+                        <div className="col flex w-full mt-8">
+                            <div className="row1 flex flex-col w-full h-48">
+                                <label htmlFor="">Message <span className="text-red-500 mb-2">*</span> </label>
+                                <textarea 
+                                    className="focus:outline-none focus:outline-slate-500 focus:border-none rounded-md border pl-2" 
+                                    rows='100%'
+                                    onChange={handelFormData}
+                                ></textarea>
+                            </div>
+                        </div>
+                        <div className="col flex w-full mt-8">
+                            <button 
+                                className="px-5 py-2 rounded-3xl text-white bg-blue-500 hover:bg-blue-300 hover:text-gray-600"
+                                onClick={handelFormSubmit}
+                            >Submit</button>
+                        </div>
+                    </form>
+                </div>
             </div>
-        </>
-    )
+            <Footer />
+        </div>
+    </>
+)
 }
