06b7a32f6dc008db3dbc63b79958d56ecb843481
suraj kumar <surajsinhakingr77>
2024-01-19 14:59:40 +0530

    done

--- pages/contact/index.jsx
+++ pages/contact/index.jsx
@@ -17,7 +17,7 @@ export default function Contact() {
         e.preventDefault();
         console.log("Does we came here to something");
         try {
-            const response = await fetch('http://localhost:3000/api/contact', {
+            const response = await fetch('https://mynewapp-peach.vercel.app/api/contact', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
