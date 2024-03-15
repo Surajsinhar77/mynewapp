29f535d8f8b5c69fa77559190ec73587fc483f2a
Suraj Kumar <surajsinhaking77@gmail.com>
2024-03-16 03:51:13 +0530

    working on the prject submittion of project on dashboard

--- components/Templateform.jsx
+++ components/Templateform.jsx
@@ -1,3 +1,4 @@
+
 import { useState } from "react";
 import { FaUserCircle } from 'react-icons/fa';
 export default function Example() {
@@ -29,21 +30,19 @@ export default function Example() {
         <form>
           <div className="space-y-12">
             <div className="border-b border-gray-900/10 pb-12">
-              <h2 className="text-base font-semibold leading-7 text-gray-900">Upload Project</h2>
+              <h2 className="text-base font-semibold leading-7 text-gray-900">Profile</h2>
               <p className="mt-1 text-sm leading-6 text-gray-600">
                 This information will be displayed publicly so be careful what you share.
               </p>
 
               <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-
-                {/* This is for the Project name   start */}
                 <div className="sm:col-span-4">
-                  <label htmlFor="Name" className="block text-sm font-medium leading-6 text-gray-900">
-                    Project Name
+                  <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
+                    Username
                   </label>
                   <div className="mt-2">
                     <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                      <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">/projects/</span>
+                      <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">workcation.com/</span>
                       <input
                         type="text"
                         name="username"
@@ -55,127 +54,6 @@ export default function Example() {
                     </div>
                   </div>
                 </div>
-                {/* This is for the Project name   End */}
-
-
-                {/* PROJECT DURACTION  START */}
-                <div className="sm:col-span-4">
-                  <label htmlFor="Name" className="block text-sm font-medium leading-6 text-gray-900">
-                    Project Period
-                  </label>
-
-                  <div className="mt-2 flex gap-x-3">
-                    {/* Input one */}
-                    <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md items-center pl-3">
-                      <label className="font-semibold text-blue-700">Start data</label>
-                      <input
-                        type="date"
-                        name="dateStart"
-                        id="dateStart"
-                        autoComplete="username"
-                        className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                        placeholder="janesmith"
-                      />
-                    </div>
-
-                    {/* Input two */}
-
-                    <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md items-center pl-3">
-                      <label className="font-semibold text-blue-700">End data</label>
-                      <input
-                        type="date"
-                        name="dateEnd"
-                        id="dateEnd"
-                        autoComplete="username"
-                        className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                        placeholder="janesmith"
-                      />
-                    </div>
-                  </div>
-                </div>
-
-
-                <div className="sm:col-span-4">
-                  <label htmlFor="Name" className="block text-sm font-medium leading-6 text-gray-900">
-                    Project tags
-                  </label>
-
-                  <div className="mt-2 flex gap-x-3">
-                    {/* Input one */}
-                    <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md items-center pl-3">
-                      <input
-                        type="text"
-                        name="dateStart"
-                        id="dateStart"
-                        autoComplete="username"
-                        className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                        placeholder="React.js"
-                      />
-                    </div>
-
-                    {/* Input two */}
-
-                       <button className="text-black px-3 border rounded">Add</button>
-
-                  </div>
-                </div>
-
-
-
-                <div className="sm:col-span-6">
-                  <label htmlFor="Name" className="block text-sm font-medium leading-6 text-gray-900">
-                    Project links
-                  </label>
-
-                  <div className="mt-2 flex gap-x-3">
-                    {/* Input one */}
-                    <div className="w-3/4 rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md items-center pl-3">
-                      <input
-                        type="text"
-                        name="dateStart"
-                        id="dateStart"
-                        autoComplete="username"
-                        className="w-full block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                        placeholder="Project link"
-                      />
-                    </div>
-                    <div className="w-3/4 rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md items-center pl-3">
-                      <input
-                        type="text"
-                        name="dateStart"
-                        id="dateStart"
-                        autoComplete="username"
-                        className="w-full block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                        placeholder="Github link"
-                      />
-                    </div>
-                  </div>
-                </div>
-
-
-                  <div className="col-span-full">
-                  <label htmlFor="cover-photo" className="block text-sm font-medium leading-6 text-gray-900">
-                    Project images
-                  </label>
-                  <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
-                    <div className="text-center">
-                      <FaUserCircle className="mx-auto h-12 w-12 text-gray-300" aria-hidden="true" />
-                      <div className="mt-4 flex text-sm leading-6 text-gray-600">
-                        <label
-                          htmlFor="file-upload"
-                          className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
-                        >
-                          <span>Upload a file</span>
-                          <input id="file-upload" name="file-upload" type="file" className="sr-only" multiple />
-                        </label>
-                        <p className="pl-1">or drag and drop</p>
-                      </div>
-                      <p className="text-xs leading-5 text-gray-600">PNG, JPG, GIF up to 10MB</p>
-                    </div>
-                  </div>
-                </div>
-
-
 
                 <div className="col-span-full">
                   <label htmlFor="about" className="block text-sm font-medium leading-6 text-gray-900">
@@ -208,42 +86,38 @@ export default function Example() {
                   </div>
                 </div>
 
+                <div className="col-span-full">
+                  <label htmlFor="cover-photo" className="block text-sm font-medium leading-6 text-gray-900">
+                    Cover photo
+                  </label>
+                  <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+                    <div className="text-center">
+                      <FaUserCircle className="mx-auto h-12 w-12 text-gray-300" aria-hidden="true" />
+                      <div className="mt-4 flex text-sm leading-6 text-gray-600">
+                        <label
+                          htmlFor="file-upload"
+                          className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
+                        >
+                          <span>Upload a file</span>
+                          <input id="file-upload" name="file-upload" type="file" className="sr-only" />
+                        </label>
+                        <p className="pl-1">or drag and drop</p>
+                      </div>
+                      <p className="text-xs leading-5 text-gray-600">PNG, JPG, GIF up to 10MB</p>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
 
-            {/* ================================================================================== */}
-
             <div className="border-b border-gray-900/10 pb-12">
-              <h2 className="text-base font-semibold leading-7 text-gray-900">Project Discription</h2>
-              {/*<p className="mt-1 text-sm leading-6 text-gray-600">Fill all possible field</p>*/}
+              <h2 className="text-base font-semibold leading-7 text-gray-900">Personal Information</h2>
+              <p className="mt-1 text-sm leading-6 text-gray-600">Use a permanent address where you can receive mail.</p>
 
               <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-
-                <div className="col-span-full">
-                  <label htmlFor="about" className="block text-sm font-medium leading-6 text-gray-900">
-                    Project Overview
-                  </label>
-                  <div className="mt-2">
-                    <textarea
-                      id="about"
-                      name="about"
-                      rows={3}
-                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                      defaultValue={''}
-                    />
-                  </div>
-                  {/*<p className="mt-3 text-sm leading-6 text-gray-600">Write a few sentences about project.</p>*/}
-                </div>
-
-
-                <div className="col-span-full">
-                  <label htmlFor="about" className="block text-sm font-medium leading-6 text-gray-900">
-                      Key feature of yours Projects
-                  </label>
-                </div>
                 <div className="sm:col-span-3">
                   <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-900">
-                    Feature one
+                    First name
                   </label>
                   <div className="mt-2">
                     <input
@@ -258,7 +132,7 @@ export default function Example() {
 
                 <div className="sm:col-span-3">
                   <label htmlFor="last-name" className="block text-sm font-medium leading-6 text-gray-900">
-                    Feature two
+                    Last name
                   </label>
                   <div className="mt-2">
                     <input
