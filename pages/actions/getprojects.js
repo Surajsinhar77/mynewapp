d6470be9c10636cc2b5f53007383c638a7a02c3a
Suraj Kumar <surajsinhaking77@gmail.com>
2024-06-17 13:19:04 +0530

    build check

--- pages/actions/getprojects.js
+++ pages/actions/getprojects.js
@@ -1,18 +1,18 @@
-import prisma from '@/pages/db/db';
+// import prisma from '@/pages/db/db';
 
 
-export async function getProjects() {
-    "use server"
-    try {
-        const projects = await prisma.projects.findMany();
+// export async function getProjects() {
+//     "use server"
+//     try {
+//         const projects = await prisma.projects.findMany();
 
-        if (!projects) {
-            return res.status(404).json({ message: "Not Project exist", projectExist: false });
-        }
+//         if (!projects) {
+//             return res.status(404).json({ message: "Not Project exist", projectExist: false });
+//         }
 
-        return res.status(200).json({ message: "All Project fetch successfully", data: projects });
-    } catch (err) {
-        console.log("this is the error here ", err);
-        return res.status(403).json({ message: err.message });
-    }
-}
+//         return res.status(200).json({ message: "All Project fetch successfully", data: projects });
+//     } catch (err) {
+//         console.log("this is the error here ", err);
+//         return res.status(403).json({ message: err.message });
+//     }
+// }
